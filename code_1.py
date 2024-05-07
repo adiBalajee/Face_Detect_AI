8d40dc7eb530d5f403b024e9e848e0a1cf53c653
adiBalajee <135713179+adiBalajee@users.noreply.github.com>
2024-05-07 02:41:07 +0000

    I've added an area for coding. My message here is what you need to add before commiting to change. Basically, a git commit creates a commit, which is like a snapshot of your repository. These commits are snapshots of your entire repository at specific times.

---
+++ code_1.py
@@ -0,0 +1,6 @@
+'''
+Hey guys! This is the code space that I have set up for the place where you can add you code. I will go over how to do 
+pull requests for updating, using VS code, and overall Github's function. This will act as the main file though!
+'''
+
+
